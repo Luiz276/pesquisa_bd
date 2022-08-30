2506a5fe6352b63d7d0048ab668e303016954f3a
Luiz Maurício <2003lmvp@gmail.com>
2022-08-30 15:43:11 -0300

    Adicionado medição de tempo de cadarequisição utilizando time_t

--- redistest.c
+++ redistest.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <hiredis/hiredis.h>
+#include <time.h>
 
 int main (int argc, char **argv) {
 redisReply *reply;
@@ -17,6 +18,23 @@ if (c->err) {
 reply = redisCommand(c,"PING %s", "Hello World");
 printf("RESPONSE: %s\n", reply->str);
 freeReplyObject(reply);
+
+// Comandos de SET e GET
+
+time_t start = time(NULL);
+
+reply = redisCommand(c,"SET %s %s","foo","bar");
+freeReplyObject(reply);
+
+printf("Time taken by SET command: %.5f\n", time(NULL)-start);
+
+start = time(NULL);
+
+reply = redisCommand(c,"GET %s","foo");
+printf("%s\n",reply->str);
+freeReplyObject(reply);
+
+printf("Time taken by GET command: %.5f\n", time(NULL)-start);
 
 redisFree(c);
 }
