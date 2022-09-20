3498ff5fe854057b41e439db614fd1b8a02b5f09
Luiz Maurício <2003lmvp@gmail.com>
2022-09-19 23:38:44 -0300

    Prototipo de paralelismo e atualizandp medicao de tempo

--- redistest.c
+++ redistest.c
@@ -3,16 +3,22 @@
 #include <string.h>
 #include <hiredis/hiredis.h>
 #include <time.h>
-
-// TODO:    -adicionar medição de tempo mais precisa como mostrado na última reunião
-//          -Adicionar possibilidade de utilização de parâmetros argc e argv
-//          -Adicionar possibilidade de dar set e get em chaves aleatórias
+#include <sys/time.h>
+#include <omp.h>
+/*
+TODO:   -Adicionar medição de tempo mais precisa como mostrado na última reunião
+        -Adicionar possibilidade de utilização de parâmetros argc e argv
+        -Adicionar possibilidade de dar set e get em chaves aleatórias
+        -Mudar calculo da chance entre get e set
+        -Paralelizar o programa
+*/
 
 int main (int argc, char **argv) {
 redisReply *reply;
 redisContext *c;
 int n_reqs = 100;
 double get_chance = 0.5;
+struct timeval t1, t2;
 
 srand(time(NULL));
 
@@ -32,32 +38,23 @@ freeReplyObject(reply);
 reply = redisCommand(c,"SET %s %s","foo","bar");
 freeReplyObject(reply);
 
+//#pragma omp parallel{ 
 for (int i=0; i<n_reqs; i++) {
+    gettimeofday(&t1, NULL);
     if (((double)rand() / (double)RAND_MAX) > get_chance) {
         reply = redisCommand(c,"SET %s %s","foo","bar");
         freeReplyObject(reply);
-        printf("set");
+        gettimeofday(&t2, NULL);
+        printf("Tempo tomado para operação SET: %f\n", (t2.tv_sec -t1.tv_sec) + ((t2.tv_usec -t1.tv_usec)/1000000.0));
     } else {
         reply = redisCommand(c,"GET %s","foo");
-        printf("%s\n",reply->str);
+        //printf("%s\n",reply->str);
         freeReplyObject(reply);
+        gettimeofday(&t2, NULL);
+        printf("Tempo tomado para operação GET: %f\n", (t2.tv_sec -t1.tv_sec) + ((t2.tv_usec -t1.tv_usec)/1000000.0));
     }
 }
-
-// time_t start = time(NULL);
-
-// reply = redisCommand(c,"SET %s %s","foo","bar");
-// freeReplyObject(reply);
-
-// printf("Time taken by SET command: %.5f\n", time(NULL)-start);
-
-// start = time(NULL);
-
-// reply = redisCommand(c,"GET %s","foo");
-// printf("%s\n",reply->str);
-// freeReplyObject(reply);
-
-// printf("Time taken by GET command: %.5f\n", time(NULL)-start);
+//}
 
 redisFree(c);
 }
