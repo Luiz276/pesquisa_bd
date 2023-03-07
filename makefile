d584fb8c99bf6cd650899e455ea8f34ac24239d5
Luiz Maurício <2003lmvp@gmail.com>
2023-03-07 14:00:12 -0300

    better scripts and .gitignore

--- makefile
+++ makefile
@@ -1,13 +1,12 @@
 #Not working properly, displays following error:
 #	makefile:2: *** faltando o separador.  Pare.
 
+CC=gcc
 BUILD_DIR := ./executable_files
 SOURCE_DIR := ./src
+REDIS := SOURCE_DIR/redis_code
 
-make: redis socket
-
-redis:
-	$(MAKE) -C SOURCE_DIR/redis_code
-
-socket:
-	$(MAKE) -C SOURCE_DIR/socket_programing
+make:
+	cd ./src/redis_code; $(CC) -o context_process context_process.c -fopenmp -lhiredis; $(CC) -o context_thread context_thread.c -fopenmp -lhiredis
+	mv ./src/redis_code/context_process ./bin
+	mv ./src/redis_code/context_thread ./bin
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/env bash
 
+#Variables
+ip=$1
+port=$2
+
 #Script that runs all source files and organizes all output files of the load generators
 #Changing directory to location of executable files
-cd ../src/redis_code;
+cd ../bin;
 
 #Running executable files
-./context_process 4 256 50 30 100000  # 4 threads com 100 requisições
-./context_thread 4 256 50 30 100000    # 4 threads com 100 requisições
+./context_process 4 256 50 30 100000 $ip $port  # 4 threads com 100 requisições
+./context_thread 4 256 50 30 100000 $ip $port   # 4 threads com 100 requisições
 
 #Creating corresponding directories
+cd ..
 mkdir -p output
-cd ../../output
+cd output
 mkdir -p cont_proc
 mkdir -p cont_thread
 
 #Moving output to respective directories
-mv ../src/redis_code/cont_proc_lat.csv ./cont_proc
-mv ../src/redis_code/cont_proc_tp.csv ./cont_proc
-mv ../src/redis_code/cont_thread_lat.csv ./cont_thread
-mv ../src/redis_code/cont_thread_tp.csv ./cont_thread
+mv ../bin/cont_proc_lat.csv ./cont_proc
+mv ../bin/cont_proc_tp.csv ./cont_proc
+mv ../bin/cont_thread_lat.csv ./cont_thread
+mv ../bin/cont_thread_tp.csv ./cont_thread
