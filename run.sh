ea437b3d39bf002a057577c7ead01ac6cbf6841e
Luiz Maurício <2003lmvp@gmail.com>
2023-03-07 14:49:38 -0300

    run.sh now compresses output folder into tar.gz

--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+#Script that runs all source files and organizes all output files of the load generators
+
+#Example: sh run.sh 127.0.0.1 6379 1000
+
 #Variables
 ip=$1
 port=$2
-reqs=1000
-
-#Script that runs all source files and organizes all output files of the load generators
+reqs=$3
 
 #Running executable files
 ./bin/context_process 4 256 50 30 $reqs $ip $port  # 4 threads com 100 requisições
@@ -13,12 +15,14 @@ reqs=1000
 
 #Creating corresponding directories
 mkdir -p output
-cd output
-mkdir -p cont_proc
-mkdir -p cont_thread
-cd ..
+mkdir -p output/cont_proc
+mkdir -p output/cont_thread
+
 #Moving output to respective directories
 mv cont_proc_lat.csv ./output/cont_proc
 mv cont_proc_tp.csv ./output/cont_proc
 mv cont_thread_lat.csv ./output/cont_thread
 mv cont_thread_tp.csv ./output/cont_thread
+
+#Tar command on output directory
+tar cvzf output.tar.gz output
